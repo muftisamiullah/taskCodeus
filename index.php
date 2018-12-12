da9c7bdd7bd3390f4e1e3f159473a0b534536b90
Sami Ullah <muftisami2010@gmail.com>
2018-12-13 00:12:34 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="FontsFree-Net-Proxima-Nova-Sbold.otf" type="text/css">
+        <title>Eventackle - HOME</title>
+  </head>
+<body>
+<nav class="navbar my-nav navbar-expand-lg navbar-light ">
+  <a class="navbar-brand" href="#">
+    <img src="images/logo.svg" width="30" height="30" class="d-inline-block align-top logo" alt="">
+  </a>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="nav navbar-nav ml-auto pt-1">
+    <li class="nav-item">
+      <button class="btn btn-primary btn-sm btn-s"><strong>Create Event</strong></button>
+    </li>
+    <li class="nav-item">
+    <button class="btn btn-outline btn-sm btn-l"><strong>Log In</strong></button>
+    </li>
+</div>
+
+
+
+
+</nav>
+<!--/.Mask-->
+
+<!-- <span class="ph-title">
+    <img class="responsive topper" src="hell.png">
+<div class="centered"><strong></strong></div>
+</span> -->
+<div class="wrapper">
+  <div class="inner">
+      <div class="content">
+          <h4 class="wow fadeInDown" data-wow-delay="1s">Bringing all the people together through live experiences!</h4>
+          <button class="wow bounceInLeft btn btn-danger btn-lg btn-search">Search</button>
+      </div>
+  </div>
+</div>
+
+<div class="events">
+  <strong><p class="p-up" style="display:inline;">Events for you in</p> <p class="p-upp" style="display:inline;">Srinagar, India</p><strong>
+    <img class="pen" src="images/ic-mode-edit-18-px.svg"></img>
+</div>
+
+<div class="container">
+  <p class="ptagsss" style="text-align:center;">Event Categories</p>
+  <div class="row">
+    <div class="col-lg-7">
+      <div class="thumbnail">
+        <img class="responsive" src="images/oil.jpg">
+      </div>
+    </div>
+    <div class="col-lg-5">
+      <div class="thumbnail">
+        <img class="responsive" src="images/mining.jpg">
+      </div> 
+    </div>
+  </div> <br>
+  <div class="row">
+    <div class="col-lg-4">
+      <div class="thumbnail">
+        <img class="responsive" src="images/energy.jpg">
+      </div>
+    </div>
+    <div class="col-lg-4">
+      <div class="thumbnail">
+        <img class="responsive" src="images/health.jpg">
+      </div> 
+    </div>
+    <div class="col-lg-4">
+      <div class="thumbnail">
+        <img class="responsive" src="images/air.jpg">
+      </div> 
+    </div>
+  </div>   
+  <br>
+  <div class="row">
+    <div class="col-lg-5">
+      <div class="thumbnail">
+        <img class="responsive" src="images/dance.jpg">
+      </div>
+    </div>
+    <div class="col-lg-7">
+      <div class="thumbnail">
+        <img class="responsive" src="images/color.jpg">
+      </div> 
+    </div>
+  </div><br>
+  <p>Show all (12)</p>
+</div> <br>
+
+
+<div class="jumbo">
+  <div class="container">
+    <strong><h3 style="text-align:center;font-size:30px;">How Eventackle Works</h3></strong>
+      <div class="row">
+        <div class="col-sm-6">
+          <span>
+            <strong><p class="ptagsss">Find Events</p></strong>
+              <p class="ptags">Discover corporate events from across the globe!</p>
+              <strong><p class="org ptags"  style="display:inline;">Find Events</p></strong> <img class="arrow" style="display:inline;" src="images/ic-arrow-downward-24-px.svg"></img>
+            </span>
+        </div>
+      <div class="col-sm-6">
+
+      <strong><p class="ptagsss">Create Event</p></strong>
+      <p class="ptags">Create beautiful event listing pages.</p>
+      <strong><p class="org ptags" style="display:inline;">Create Events</p></strong> <img class="arrow" style="display:inline;" src="images/ic-arrow-downward-24-px.svg"></img>
+      </div>
+   </div>
+  </div>
+</div>
+
+<br>
+<br>
+
+<div class="contain">
+  <div class="row text-left">
+    <div class="col-sm-3">
+      <strong><p class="ptagss">Use Eventackle</p></strong>
+      <p class="ptags">How it works</p>
+      <p class="ptags">FAQ</p>
+      <p class="ptags">Sitemap</p>
+    </div>
+    <div class="col-sm-3">
+    <strong><p class="ptagss">For Organisers</p></strong>
+      <p class="ptags">List your event</p>
+      <p class="ptags">Testimonials</p>
+      <p class="ptags">Benefits</p>
+      </div>
+    <div class="col-sm-3">
+    <strong><p class="ptagss">Events</p></strong>
+      <p class="ptags">About Us</p>
+      <p class="ptags">Careers</p>
+      <p class="ptags">Blog</p>
+      </div>
+    <div class="col-sm-3">
+    <strong><p class="ptagss">Social</p></strong>
+      <p class="ptags"><img class="facebook" src="images/facebook.svg"></img></p>
+      <p class="ptags"><img class="facebook" src="images/twitter.svg"></img></p>
+      <p class="ptags"><img class="facebook" src="images/instagram.svg"></img></p>
+  </div>
+  </div>
+</div>
+<br>
+<br>
+
+<div class="footer">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light Rectangle-7">
+  <a class="navbar-brand" href="#">
+    <img src="images/logo-copy.svg" width="30" height="30" class="d-inline-block align-top logo" alt="">
+  </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto footer-ul">
+      <li class="nav-item">
+          <a class="nav-link footer-a">© 2018, Eventackle</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link footer-a" href="">Cookies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link footer-a" href="">Terms & Privacy</a>
+        </li>
+      </ul>        
+    </div>
+  </nav>
+</div>
